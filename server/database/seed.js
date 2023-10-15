ccecbc8b815531533efdcba36e1d4f10da2f040f
Raktim Shrestha <raktim@rumsan.com>
2023-10-15 19:27:08 +0545

    adding fake seed data

--- server/database/seed.js
+++ server/database/seed.js
@@ -37,7 +37,7 @@ const setup = {
     console.log("Finishing multiple Categories...");
 
     console.log("adding Products...");
-    const product1 = await productController.create({
+    await productController.create({
       name: "Armani tshirt",
       alias: "asu, hero",
       description: "Best tees in the town",
@@ -52,8 +52,120 @@ const setup = {
       ],
       category: category1._id,
     });
-    const product2 = await productController.create({
+    await productController.create({
       name: "zara Jeans",
+      description: "Best pants in the town",
+      type: "jeans",
+      price: 3000,
+      isRelease: true,
+      quantity: 5,
+      images: [
+        "https://thumbs.dreamstime.com/b/womens-blue-denim-jeans-4462309.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStvIbWpN9VnfX-HxPAYfnxDgHH-ulWlFMrYA&usqp=CAU",
+      ],
+      category: category2._id,
+    });
+    await productController.create({
+      name: "Baka tshirt",
+      alias: "asu, hero",
+      description: "Best tees in the town",
+      type: "tshirt",
+      price: 2000,
+      isRelease: true,
+      quantity: 4,
+      images: [
+        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+        "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+      ],
+      category: category1._id,
+    });
+    await productController.create({
+      name: "Baka Jeans",
+      description: "Best pants in the town",
+      type: "jeans",
+      price: 3000,
+      isRelease: true,
+      quantity: 5,
+      images: [
+        "https://thumbs.dreamstime.com/b/womens-blue-denim-jeans-4462309.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStvIbWpN9VnfX-HxPAYfnxDgHH-ulWlFMrYA&usqp=CAU",
+      ],
+      category: category2._id,
+    });
+    await productController.create({
+      name: "Cat tshirt",
+      alias: "asu, hero",
+      description: "Best tees in the town",
+      type: "tshirt",
+      price: 2000,
+      isRelease: true,
+      quantity: 4,
+      images: [
+        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+        "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+      ],
+      category: category1._id,
+    });
+    await productController.create({
+      name: "Cat Jeans",
+      description: "Best pants in the town",
+      type: "jeans",
+      price: 3000,
+      isRelease: true,
+      quantity: 5,
+      images: [
+        "https://thumbs.dreamstime.com/b/womens-blue-denim-jeans-4462309.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStvIbWpN9VnfX-HxPAYfnxDgHH-ulWlFMrYA&usqp=CAU",
+      ],
+      category: category2._id,
+    });
+    await productController.create({
+      name: "Dog tshirt",
+      alias: "asu, hero",
+      description: "Best tees in the town",
+      type: "tshirt",
+      price: 2000,
+      isRelease: true,
+      quantity: 4,
+      images: [
+        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+        "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+      ],
+      category: category1._id,
+    });
+    await productController.create({
+      name: "Dog Jeans",
+      description: "Best pants in the town",
+      type: "jeans",
+      price: 3000,
+      isRelease: true,
+      quantity: 5,
+      images: [
+        "https://thumbs.dreamstime.com/b/womens-blue-denim-jeans-4462309.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStvIbWpN9VnfX-HxPAYfnxDgHH-ulWlFMrYA&usqp=CAU",
+      ],
+      category: category2._id,
+    });
+    await productController.create({
+      name: "Elephant tshirt",
+      alias: "asu, hero",
+      description: "Best tees in the town",
+      type: "tshirt",
+      price: 2000,
+      isRelease: true,
+      quantity: 4,
+      images: [
+        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+        "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+      ],
+      category: category1._id,
+    });
+    await productController.create({
+      name: "Elephant Jeans",
       description: "Best pants in the town",
       type: "jeans",
       price: 3000,
--- server/modules/products/product.controller.js
+++ server/modules/products/product.controller.js
@@ -8,7 +8,7 @@ const create = (payload) => {
 
 const list = async (limit, page, search) => {
   const pageNum = parseInt(page) || 1;
-  const size = parseInt(limit) || 5;
+  const size = parseInt(limit) || 20;
   const query = [];
   if (search?.name) {
     query.push({
